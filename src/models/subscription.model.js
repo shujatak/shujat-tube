d39a4008885fc4bd4305015acfb3ed5bc19d003b
shujatak <shujatalikhan504@gmail.com>
2024-08-09 23:20:08 +0500

    Add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId, // One who is subscribing
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId, // Channel being subscribed
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
